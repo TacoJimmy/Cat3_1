84eaa7e223b37188555e1302c04134ab291d6c6c
TacoJimmy <t5669016@ntut.org.tw>
2023-11-27 11:12:59 +0800

    Update Mqtt_test.py

--- Mqtt_test.py
+++ Mqtt_test.py
@@ -27,9 +27,10 @@ if __name__ == '__main__':
         time.sleep(10)
         '''
         print ("Station")
-        client = mqtt.Client('', True, None, mqtt.MQTTv31)
-        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        client.on_connect
+        #client = mqtt.Client('', True, None, mqtt.MQTTv31)
+        #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        client = mqtt.Client()
+        #client.on_connect
         client.username_pw_set('fQdc44pz','infilink')
         client.connect('mqtt-device.fetiot3s1.fetnet.net', 8884, 60)
         #power_t = random.randint(1,10)
