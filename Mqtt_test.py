2649ac2e35cc50dabd693a4b192049b57f203d61
TacoJimmy <t5669016@ntut.org.tw>
2023-11-27 10:40:24 +0800

    Update Mqtt_test.py

--- Mqtt_test.py
+++ Mqtt_test.py
@@ -13,13 +13,23 @@ import time
 
 if __name__ == '__main__':  
     while True:
+        print ("Production")
         client = mqtt.Client()
         client.on_connect
         client.username_pw_set('3whDyeH9','infilink')
+        client.connect('mqtt-device.fetiot3p1.fetnet.net', 8884, 60)
+        #power_t = random.randint(1,10)
+        
+        payload_iaq = {"test":1000} #json
+        print(client.publish("smartbuilding/v1/telemetry/nh220", json.dumps(payload_iaq)))
+        time.sleep(10)
+        print ("Station")
+        client = mqtt.Client()
+        client.on_connect
+        client.username_pw_set('fQdc44pz','infilink')
         client.connect('mqtt-device.fetiot3s1.fetnet.net', 8884, 60)
         #power_t = random.randint(1,10)
         
         payload_iaq = {"test":1000} #json
         print(client.publish("smartbuilding/v1/telemetry/nh220", json.dumps(payload_iaq)))
         time.sleep(10)
-    
